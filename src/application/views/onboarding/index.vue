edb5b524cc4a30c1d28ac09bce369db1f23c77f5
kromate <akpananthony33@gmail.com>
2021-09-30 23:28:22 +0100

    feat: onboarding

---
+++ src/application/views/onboarding/index.vue
@@ -0,0 +1,44 @@
+
+<template>
+	<swiper :pagination="true" class="mySwiper">
+		<swiper-slide>Slide 1</swiper-slide>
+		<swiper-slide>Slide 2</swiper-slide>
+		<swiper-slide>Slide 3</swiper-slide>
+		<swiper-slide>Slide 4</swiper-slide>
+	</swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+	Pagination
+} from 'swiper'
+
+// install Swiper modules
+SwiperCore.use([Pagination])
+
+
+export default {
+	components: {
+		Swiper,
+		SwiperSlide,
+	},
+	data() {
+		return {
+        
+        
+		}
+	},
+	methods: {
+    
+	}
+  
+}
+</script>
---
+++ src/application/views/onboarding/slideOne.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex container flex-col bg-dark h-screen w-screen justify-center items-center">
+		<img src="../../assets/images/onboarding/askAQuestion.svg" alt="ask a question" class="object-fit w-2/4">
+
+		<ion-text class="text-xl text-white font-extrabold">
+			<h1>Ask a Question</h1>
+		</ion-text>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
