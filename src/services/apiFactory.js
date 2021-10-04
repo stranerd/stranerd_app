966012972b3a134d1a4a96d98506c36d96d84fdc
kromate <akpananthony33@gmail.com>
2021-10-04 23:32:43 +0100

    update

--- src/services/apiFactory.js
+++ src/services/apiFactory.js
@@ -6,3 +6,15 @@ import {
 	$AuthUser,
 	$simpleAuthHeader
 } from './orgRepository'
+
+
+const apiFactory = {
+//   $_getLinchpin() {
+//     const url = "/auth/refresh_linchpin";
+//     return AxiosAAA.get(url);
+//   },
+//   $_getPrivileges() {
+//     const url = `/roles/${$AuthUser.roles.humanar.roleId}/users`;
+//     return AxiosAAA.get(url);
+//   },
+}
