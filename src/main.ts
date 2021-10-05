34f3806991c879b5d7c03cf74c0ed0582103bc05
kromate <akpananthony33@gmail.com>
2021-10-05 23:45:56 +0100

    vite

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { createRouter, createWebHistory } from '@ionic/vue-router'
 import { IonicVue } from '@ionic/vue'
 
 import { store, key } from './application/store'
-import apiFactory from '@/services/apiFactory.js'
+
+// import apiFactory from './services/apiFactory'
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css'
@@ -56,6 +57,7 @@ const router = Promise.all(routes).then((routes) => {
 
 const init = async() => {
 	createApp({
+		// mixins: [apiFactory],
 		components: {
 			App
 		},
