e113c01a0d3d3ee3b829995b1760721dcbc4b693
kromate <akpananthony33@gmail.com>
2021-10-06 06:37:48 +0100

    feat: home page

--- src/application/views/dashboard/^home.vue
+++ src/application/views/dashboard/^home.vue
@@ -1,22 +1,49 @@
 <template>
 	<ion-page>
-		<ion-header mode="ios">
-			<ion-toolbar>
-				<ion-title>Home</ion-title>
-			</ion-toolbar>
-		</ion-header>
-		<ion-content :fullscreen="true">
-					
+		<ion-content :fullscreen="true" class=" mx-auto">
+			<div class="w-full flex justify-between">
+				<span class="text-[24px] font-bold text-sub">
+					Featured tutors
+				</span>
+
+				<div class="flex justify-center items-center ">
+					<a class="commentIcons" to="#" >
+						<ion-icon :icon="chevronBackOutline" class="text-2xl"/>
+					</a>
+					<a
+						v-for="(comment,index) in 3"
+						:key="'comment'+ index"
+						:class="currentIndex === index ? 'commentIconsDotActive' : 'commentIconsDot'"
+							
+					>
+						<i class="fas fa-circle" />
+					</a>
+					<a class="commentIcons" to="#" @click.prevent="goToNext">
+						<ion-icon :icon="chevronForwardOutline" class="text-2xl"/>
+					</a>
+				</div>
+
+				<div class="text-primary flex items-center font-bold text-base">
+					<span>view all</span>
+					<ion-icon :icon="chevronForwardOutline" class="text-2xl"></ion-icon>	
+				</div>
+			</div>
 		</ion-content>
 	</ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+import { IonPage,  IonContent, IonIcon } from '@ionic/vue'
+import { chevronForwardOutline, chevronBackOutline } from 'ionicons/icons'
 
 export default  {
 	name: 'Home',
-	components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+	components: { IonContent, IonPage, IonIcon },
+	setup(){
+		return{
+			chevronForwardOutline, chevronBackOutline
+		}
+	}
 }
 </script>
 <style scoped>
