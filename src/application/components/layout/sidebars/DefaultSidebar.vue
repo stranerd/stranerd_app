50a6f2903b689091e6f2b2ce4660ee5c83bcd4e8
kromate <akpananthony33@gmail.com>
2021-10-07 17:12:37 +0100

    feat; separate

---
+++ src/application/components/layout/sidebars/DefaultSidebar.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class=" layout-side-left ">
+
+		<div class="py-5  pl-11 flex flex-row items-center">
+			<img src="/assets/images/logo.svg" class="w-32" />
+		</div>
+
+		<div class="flex flex-col pl-8 mt-6">
+			
+			<nav-element :icon="home" :route="route" tab="home" ></nav-element>
+
+			<nav-element :icon="helpCircle" :route="route" tab="questions" ></nav-element>
+
+			<nav-element :icon="people" :route="route" tab="tutors" ></nav-element>
+
+			<nav-element :icon="calendarClear" :route="route" tab="schedule" ></nav-element>
+
+			<nav-element :icon="wallet" :route="route" tab="wallet" ></nav-element>
+
+		</div>
+		
+	</div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+import { home, helpCircle, people , chatbubble, add, calendarClear, wallet } from 'ionicons/icons'
+const NavElement = defineAsyncComponent(() => import('@/application/components/layout/sidebars/SideBarElement.vue'))
+
+export default {
+	name: 'DefaultSidebar',
+	components: {  NavElement },
+	setup() {
+		return {
+			home, 
+			helpCircle, 
+			people,
+			chatbubble,
+			add,
+			calendarClear,
+			wallet
+		}
+	}
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/application/components/layout/sidebars/SideBarElement.vue
@@ -0,0 +1,50 @@
+<template>
+	<router-link :to="`/dashboard/${props.tab}`" class="flex flex-col rounded-l-[150px] text-icon_inactive cursor-pointer text-sm hover:text-dark_gray mb-2">
+		<div :class="`${props.route.name == props.tab ? 'flex' : 'hidden' } flex bg-white flex-row-reverse`">
+			<div class="w-1/2 rounded-br-[150px] py-2 bg-light_gray">
+			</div>
+			<div class="w-1/2  py-2 bg-light_gray">
+			</div>
+		</div>
+		<div :class="`py-3 flex flex-row px-4 items-center ${props.route.name == props.tab ? 'text-dark_gray bg-white' : '' }   rounded-l-3xl`">
+			<ion-icon :icon="props.icon" class="text-[21px] mr-4" ></ion-icon>
+			<span class="font-semibold  capitalize">{{props.tab}}</span>
+		</div>
+		<div :class="`${props.route.name == props.tab ? 'flex' : 'hidden' }  bg-white flex-row-reverse`">
+			<div class="w-1/2 rounded-tr-[150px] py-2 bg-light_gray">
+			</div>
+			<div class="w-1/2  py-2 bg-light_gray">
+			</div>
+		</div>
+	</router-link>
+</template>
+<script lang="ts">
+import { defineComponent,  Ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { IonIcon } from '@ionic/vue'
+import { home, helpCircle, people , chatbubble, add, calendarClear, wallet } from 'ionicons/icons'
+
+export default defineComponent({
+	components: { IonIcon },
+	setup(props) {
+		const route = useRoute()
+		return {
+			home, 
+			helpCircle, 
+			people,
+			chatbubble,
+			add,
+			route,
+			calendarClear,
+			wallet,
+			props
+		}
+	},
+	props: {
+		// route: {},
+		tab: String,
+		icon: {}
+	},
+
+})
+</script>
