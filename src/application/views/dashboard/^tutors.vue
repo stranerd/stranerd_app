107b24326565b0d8739ae5d8d418946ba23e525a
kromate <akpananthony33@gmail.com>
2021-10-09 02:31:14 +0100

    final-edit

--- src/application/views/dashboard/^tutors.vue
+++ src/application/views/dashboard/^tutors.vue
@@ -1,12 +1,12 @@
 <template>
 	<ion-page class="pt-8">
 		<ion-content :fullscreen="true">
-			<div class="mx-auto w-[48%] ">
+			<div class="mx-auto min-w-[48%] max-w-[550px] px-3">
 				<div class="bg-light_gray rounded-md flex flex-row items-center">
 					<button class="py-3 px-3 text-xs md:text-sm  font-semibold text-white bg-icon_inactive rounded-md w-1/2">
 						Explore tutors
 					</button>
-					<button class="py-3 px-3 text-xs md:text-sm  font-semibold bg-light_gray text-dark_gray rounded-md w-1/2">
+					<button class="py-3 px-3 text-xs md:text-sm  font-semibold bg-light_gray text-dark_gray rounded-md w-1/2 ">
 						Find a tutor
 					</button>
 				</div>
