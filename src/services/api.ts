15ad833b2a87278cbc36b32c8a8f56062bb2e95d
kromate <akpananthony33@gmail.com>
2021-10-18 15:22:08 +0100

    feat: added all the apis in he google docs

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,8 +7,52 @@ export	const signIn = (payload: any)=> {
 	const url = '/emails/signin'
 	return AxiosAuth.post(url, payload)
 }
+export	const signUp = (payload: any)=> {
+	const url = '/emails/signup'
+	return AxiosAuth.post(url, payload)
+}
+export	const sendVerificationMail = (payload: any)=> {
+	const url = '/emails/sendVerificationMail'
+	return AxiosAuth.post(url, payload)
+}
+export	const verify = (payload: any)=> {
+	const url = '/emails/verify'
+	return AxiosAuth.post(url, payload)
+}
+export	const token = (payload: any)=> {
+	const url = '/token'
+	return AxiosAuth.post(url, payload)
+}
+export	const roles = (payload: any)=> {
+	const url = '/roles'
+	return AxiosAuth.post(url, payload)
+}
+export	const getUser = (payload: any)=> {
+	const url = '/user'
+	return AxiosAuth.get(url)
+}
+export	const updateUser = (payload: any)=> {
+	const url = '/user'
+	return AxiosAuth.put(url, payload)
+}
 export const signOut= ()=> {
-	const url = '/emails/signout'
+	const url = '/signout'
+	return AxiosAuth.post(url)
+}
+export const sendResetMail= ()=> {
+	const url = '/passwords/sendResetMail'
+	return AxiosAuth.post(url)
+}
+export const resetPassword= ()=> {
+	const url = '/passwords/reset'
+	return AxiosAuth.post(url)
+}
+export const updatePassword= ()=> {
+	const url = '/passwords/update'
+	return AxiosAuth.post(url)
+}
+export const googleAuth = ()=> {
+	const url = '/identities/google'
 	return AxiosAuth.post(url)
 }
 
