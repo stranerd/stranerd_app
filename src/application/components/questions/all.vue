d07b867bb0a48095025647a3492f34359cf086f9
kevinand11 <kevinfizu@gmail.com>
2021-11-05 12:36:31 +0100

    fix: empty state of questions

--- src/application/components/questions/all.vue
+++ src/application/components/questions/all.vue
@@ -7,11 +7,11 @@
 				<div
 					class="py-1 px-2 px-md text-sm col-span-12 md:col-start-4 md:col-end-10 md:mb-3   bg-light_gray rounded-xl md:rounded-md flex flex-row">
 					<ion-select v-model="subjectId"
-						class="w-full text-xs md:text-sm text-icon_inactive  placeholder-[#8B9EB1] font-bold"
-						interface="action-sheet" placeholder="Filter by subject">
+								class="w-full text-xs md:text-sm text-icon_inactive  placeholder-[#8B9EB1] font-bold"
+								interface="action-sheet" placeholder="Filter by subject">
 						<ion-select-option :value="''">All Subjects</ion-select-option>
 						<ion-select-option v-for="(subject, index) in subjects" :key="index + 'subject'"
-							:value="subject.id">{{ subject.name }}
+										   :value="subject.id">{{ subject.name }}
 						</ion-select-option>
 					</ion-select>
 				</div>
@@ -19,21 +19,21 @@
 				<div
 					class="mt-2 col-span-12  md:col-start-4 md:col-end-10 flex flex-row-reverse  justify-center items-center ">
 					<div class="flex flex-row items-center mr-2 cursor-pointer"
-						@click="answered = answeredChoices[2].val; selectedFilter = 'All'">
+						 @click="answered = answeredChoices[2].val; selectedFilter = 'All'">
 						<ion-icon :icon=" selectedFilter == 'All' ? ellipse : ellipseOutline"
-							class="text-lg mr-1 text-primary"></ion-icon>
+								  class="text-lg mr-1 text-primary"></ion-icon>
 						<span class="font-bold text-icon_inactive">Answered </span>
 					</div>
 					<div class="flex flex-row items-center mr-2 cursor-pointer"
-						@click="answered = answeredChoices[3].val; selectedFilter = 'Answered'">
+						 @click="answered = answeredChoices[3].val; selectedFilter = 'Answered'">
 						<ion-icon :icon=" selectedFilter == 'Answered' ? ellipse : ellipseOutline"
-							class="text-lg mr-1 text-primary"></ion-icon>
+								  class="text-lg mr-1 text-primary"></ion-icon>
 						<span class="font-bold text-icon_inactive">Unanswered </span>
 					</div>
 					<div class="flex flex-row items-center mr-2 cursor-pointer"
-						@click="answered = answeredChoices[0].val; selectedFilter = 'Unanswered'">
+						 @click="answered = answeredChoices[0].val; selectedFilter = 'Unanswered'">
 						<ion-icon :icon=" selectedFilter == 'Unanswered' ? ellipse : ellipseOutline"
-							class="text-lg mr-1 text-primary"></ion-icon>
+								  class="text-lg mr-1 text-primary"></ion-icon>
 						<span class="font-bold text-icon_inactive">All </span>
 					</div>
 					<span class="font-bold text-icon_inactive mr-3">Sort by: </span>
@@ -47,13 +47,13 @@
 					<empty-state
 						:btnText="'Ask a question'"
 						:info="'No questions found! Start asking questions to help with homework and studying.'"
-						:route="'/dashboard/question'"
+						:route="'/dashboard/questions'"
 					></empty-state>
 				</template>
 				<template v-else>
 					<div v-for="(question,index) in questions"
-						:key="index"
-						:class="0 === index ? 'w-full md:px-2 mb-3' :  'md:w-1/2 lg:w-full w-full md:px-2 md:py-3 mb-4 md:mb-0'">
+						 :key="index"
+						 :class="0 === index ? 'w-full md:px-2 mb-3' :  'md:w-1/2 lg:w-full w-full md:px-2 md:py-3 mb-4 md:mb-0'">
 						<question
 							:colorClass="0 === index ? 'bg-butter_yellow h-[230px] md:h-[220px]' : 'bg-light_gray h-[230px] md:h-[220px]'"
 							:isFeatured="0 === index ? true : false" :question="question" />
@@ -67,13 +67,11 @@
 
 </template>
 <script lang="ts">
-import { IonSelect, IonSelectOption, IonIcon } from '@ionic/vue'
+import { IonIcon, IonSelect, IonSelectOption } from '@ionic/vue'
 import { defineAsyncComponent, defineComponent, ref } from 'vue'
-import { useRouter } from 'vue-router'
 
 import { ellipse, ellipseOutline } from 'ionicons/icons'
 // Import Swiper Vue.js components
-
 // Import Swiper styles
 import 'swiper/swiper-bundle.min.css'
 import { useQuestionList } from '@app/composable/questions/questions'
@@ -88,7 +86,7 @@ export default defineComponent({
 		const {
 			filteredQuestions: questions, error, loading, hasMore,
 			answeredChoices, answered, subjectId,
-			fetchOlderQuestions,
+			fetchOlderQuestions
 		} = useQuestionList()
 		const { subjects } = useSubjectList()
 		const selectedFilter = ref('All')
