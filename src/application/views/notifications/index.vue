0aee7a7b42b428a2ddb6e48647530f36da8ed211
kromate <akpananthony33@gmail.com>
2021-11-14 22:47:52 +0100

    feat: icons

--- src/application/views/notifications/index.vue
+++ src/application/views/notifications/index.vue
@@ -53,6 +53,7 @@ import { IonContent, IonIcon, IonPage } from '@ionic/vue'
 import { checkmarkDone } from 'ionicons/icons'
 import PageLoading from '@app/components/core/PageLoading.vue'
 import EmptyState from '@app/components/core/EmptyState.vue'
+import { useAuth } from '@app/composable/auth/auth'
 
 export default defineComponent({
 	name: 'Notification',
@@ -61,7 +62,8 @@ export default defineComponent({
 	middlewares: ['isAuthenticated'],
 	components: { NotificationCard, IonContent, IonPage, IonIcon, PageLoading, EmptyState },
 	setup () {
-		const { notifications, error, loading, listener, hasMore, fetchOlderNotifications } = useNotificationList()
+		const { id } = useAuth()
+		const { notifications, error, loading, listener, hasMore, fetchOlderNotifications } = useNotificationList(id.value)
 		onMounted(() => {
 			if (!listener.isRunning.value) listener.startListener()
 		})
