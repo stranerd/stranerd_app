84ed2572f2bb956e63bdc4c90d4c56019394a4e5
Anthony Akpan <52849936+kromate@users.noreply.github.com>
2021-12-05 18:54:58 +0000

    feat: updat

--- src/application/components/modals/study/AddSet.vue
+++ src/application/components/modals/study/AddSet.vue
@@ -4,8 +4,16 @@
 			Add items from my study
 		</template>
 
-		<ion-select v-model="selectedSet" >
-			<ion-select-option v-for="set in sets"  :key="set.id" :value="set.id">{{ set.name }}</ion-select-option>   
+		<ion-select v-model="selectedSet" interface="action-sheet" class="border border-main_dark rounded-xl px-4 lg:mx-4 mt-4" placeholder="FlashCards">
+			<ion-select-option v-for="flashcard in flashCards"  :key="flashcard.id" :value="flashcard.id" class="border border-main_dark">{{ flashcard.title }}</ion-select-option>   
+		</ion-select>
+
+		<ion-select v-model="selectedSet" interface="action-sheet" class="border border-main_dark rounded-xl px-4 lg:mx-4 mt-4" placeholder="TestPreps">
+			<ion-select-option v-for="testPrep in testPreps"  :key="testPrep.id" :value="testPrep.id" class="border border-main_dark">{{ testPrep.name }}</ion-select-option>   
+		</ion-select>
+
+		<ion-select v-model="selectedSet" interface="action-sheet" class="border border-main_dark rounded-xl px-4 lg:mx-4 mt-4" placeholder="Notes">
+			<ion-select-option v-for="note in notes"  :key="note.id" :value="note.id" class="border border-main_dark">{{ note.title }}</ion-select-option>   
 		</ion-select>
 	</Modal>
 </template>
@@ -40,3 +48,31 @@ export default defineComponent({
 	}
 })
 </script>
+
+
+<style scoped>
+	ion-textarea {
+		--placeholder-color: #8B9EB1;
+	}
+
+	ion-select {
+
+		/* Set a different placeholder color */
+		--placeholder-color: #132740;
+		font-weight: bold;
+
+		/* Set full opacity on the placeholder */
+		--placeholder-opacity: 1;
+	}
+
+	ion-input {
+
+		/* Set a different placeholder color */
+		--placeholder-color: #8B9EB1;
+
+		/* Set full opacity on the placeholder */
+		--placeholder-opacity: 1;
+	}
+
+
+</style>
