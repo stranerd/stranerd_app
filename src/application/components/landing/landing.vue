b25343822b6c496a1c6dd71c65c26e9e9bb00d5b
kromate <akpananthony33@gmail.com>
2021-12-21 23:33:04 +0100

    feat: started work on landing

--- src/application/components/landing/landing.vue
+++ src/application/components/landing/landing.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="w-full relative bg-primary min-h-[80vh] flex justify-center items-center ">
-		<div class="flex justify-center items-center mx-auto max-w-[80rem]">
-			<div class="w-5/12 flex flex-col"> 
+	<div class="w-full relative bg-primary min-h-[80vh] flex justify-between items-center ">
+		<div class="flex justify-center items-center mx-auto max-w-[100rem]">
+			<div class="w-6/12 flex flex-col"> 
 				<ion-text class="text-white text-5xl font-bold">
 					Start STUDYING
 					BETTER. SMARTER. FASTER.
@@ -10,10 +10,21 @@
 				<ion-text class="text-2xl text-white">
 					With Test Prep, Flashcards, Notes and Expert Answers.
 				</ion-text>
+				<ion-button class="btn-outline w-80 text-2xl block">
+					Get Started Now
+				</ion-button>
+
+				<ion-text class="text-2xl text-white mt-8">
+					or download the app instead
+				</ion-text>
+				<div class="flex gap-4">
+					<img alt="" class="object-contain h-[100%]" src="@app/assets/images/auth/playstore.svg">
+					<img alt="" class="object-contain h-[100%]" src="@app/assets/images/auth/appstore.svg">
+				</div>
 			</div>
 
-			<div class="w-7/12">
-				<img alt="" class="object-contain h-[65%]" src="@app/assets/images/auth/auth.svg">
+			<div class="w-6/12">
+				<img alt="" class="object-contain h-[100%]" src="@app/assets/images/auth/auth.svg">
 			</div>
 		</div>
 	
@@ -28,3 +39,10 @@ export default {
 </script>
 
 
+<style scoped>
+ion-button{
+	--padding-top: 2.125rem !important;
+	--padding-bottom: 2.125rem !important;
+	--border-width: 2px !important
+}
+</style>
