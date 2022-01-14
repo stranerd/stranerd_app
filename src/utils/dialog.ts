e2dd711e2318a69de1ad0d3fb82f4bd75f434f7e
kevinand11 <kevinfizu@gmail.com>
2022-01-14 16:28:42 +0100

    fix: cleanup

--- src/utils/dialog.ts
+++ src/utils/dialog.ts
@@ -1,12 +1,21 @@
 import { Toast } from '@capacitor/toast'
 import { Dialog } from '@capacitor/dialog'
+import SweetAlert from 'sweetalert2'
+import { isWeb } from '@utils/constants'
 
 type ToastArgs = {
 	title: string
 }
 
 export const Notify = async (args: ToastArgs) => {
-	await Toast.show({
+	isWeb ? await SweetAlert.fire({
+		title: args.title,
+		icon: 'info',
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 3000
+	}) : await Toast.show({
 		text: args.title,
 		position: 'top',
 		duration: 'short'
@@ -19,7 +28,16 @@ type AlertArgs = ToastArgs & {
 }
 
 export const Alert = async (args: AlertArgs) => {
-	const { value } = await Dialog.confirm({
+	const { value } = isWeb ? await SweetAlert.fire<boolean>({
+		title: args.title,
+		text: args.title,
+		icon: 'info',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: args.confirmButtonText,
+		cancelButtonText: args.cancelButtonText ?? 'Cancel'
+	}) : await Dialog.confirm({
 		title: args.title,
 		message: args.title,
 		okButtonTitle: args.confirmButtonText,
