c9aaad698fdf338f6c53448ca40cef0d8ca74d2b
kevinand11 <kevinfizu@gmail.com>
2022-01-14 12:03:03 +0100

    fix: break word

--- src/application/components/core/text/DisplayHtml.vue
+++ src/application/components/core/text/DisplayHtml.vue
@@ -1,5 +1,5 @@
 <template>
-	<pre class="whitespace-pre-wrap break-all font-rale editor-body" v-html="value" />
+	<pre class="font-rale body" v-html="value" />
 </template>
 
 <script lang="ts">
@@ -19,3 +19,26 @@ export default defineComponent({
 	}
 })
 </script>
+
+<style lang="scss" scoped>
+	.body {
+		white-space: pre-wrap;
+		word-wrap: break-word;
+		word-break: break-word;
+		overflow-wrap: break-word;
+
+		p {
+			margin-bottom: 0.25rem;
+		}
+
+		& > p:last-child {
+			margin-bottom: 0;
+		}
+
+		* {
+			word-wrap: break-word;
+			word-break: break-word;
+			overflow-wrap: break-word;
+		}
+	}
+</style>
--- src/application/components/study/flashCards/FlashCardListView.vue
+++ src/application/components/study/flashCards/FlashCardListView.vue
@@ -6,10 +6,10 @@
 
 		<div v-for="(card, index) in flashCard.set" :key="index"
 			class="flex justify-between  w-full md:mt-6 mt-3 text-body">
-			<div class=" bg-white py-3 px-6 rounded-xl w-1/2 md:mr-6 mr-3">
+			<div class="bg-white py-3 px-6 rounded-xl w-1/2 md:mr-6 mr-3">
 				<DisplayHtml :html="card.question" />
 			</div>
-			<div class=" bg-white  py-3 px-6 rounded-xl w-1/2">
+			<div class="bg-white py-3 px-6 rounded-xl w-1/2">
 				<DisplayHtml :html="card.answer" />
 			</div>
 		</div>
