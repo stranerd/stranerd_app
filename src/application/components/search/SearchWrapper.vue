521e1173cc2a62ccc647ff9bccbb4d5aebb38da5
kevinand11 <kevinfizu@gmail.com>
2022-01-19 22:57:06 +0100

    fix: searchbar

--- src/application/components/search/SearchWrapper.vue
+++ src/application/components/search/SearchWrapper.vue
@@ -8,7 +8,7 @@
 						Explore thousands of study materials
 					</ion-text>
 					<ion-searchbar v-model.trim="searchTerm"
-						:debounce="700"
+						:disabled="loading"
 						class="lg:w-7/12 w-11/12 mb-8 mt-5 lg:mt-0"
 						placeholder="Search for anything..."
 						showCancelButton="never"
