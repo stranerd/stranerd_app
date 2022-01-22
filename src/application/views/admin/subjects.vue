b2328acc66a99b62f4c3cdfe2e4222a959a0abbe
kromate <akpananthony33@gmail.com>
2022-01-22 13:43:40 +0100

    feat: added the add admin page

---
+++ src/application/views/admin/subjects.vue
@@ -0,0 +1,26 @@
+<template>
+	<AdminWrapper>
+		<div class="mb-12">
+
+
+	
+		</div>
+	</AdminWrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AdminWrapper from '@root/application/components/admin/AdminWrapper.vue'
+
+
+export default defineComponent({
+	name: 'AdminSubjectPage',
+	displayName: 'Administration',
+	components: {
+		AdminWrapper,
+
+	}
+})
+</script>
+
+
