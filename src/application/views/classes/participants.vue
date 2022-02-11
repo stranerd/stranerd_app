a1086997385207ae5b1c966f8d486252f60341bd
kromate <akpananthony33@gmail.com>
2022-02-11 22:37:14 +0100

    feat: update

--- src/application/views/classes/participants.vue
+++ src/application/views/classes/participants.vue
@@ -4,16 +4,34 @@
 		<h1>
 			classParticipants
 		</h1>
+
+		<div>
+			<div class="bg-white p-6 rounded-xl mt-4" v-for="n in 3" :key="n">
+				<div class="flex items-center">
+					<Avatar  :size="48" src="#" />
+					<div class="flex flex-col items-start font-bold ml-4">
+						<ion-text class="text-base text-main_dark">
+							Timmy Neutron
+						</ion-text>
+						<tag :index="2" :secondary="true" tag="Comrade"/>
+					</div>
+				</div>
+
+		
+			
+			</div>
+		</div>
 	</classWrapper>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import classWrapper from '@app/components/classes/ClassWrapper.vue'
+import Tag from '@root/application/components/core/Tag.vue'
 
 export default defineComponent({
 	name: 'classParticipants',
 	displayName: 'Classes',
-	components: { classWrapper }
+	components: { classWrapper, Tag }
 })
 </script>
