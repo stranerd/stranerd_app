d6bf1430dab522a7ee8e1dde98fcd7944860c77e
kevinand11 <kevinfizu@gmail.com>
2022-02-19 20:55:38 +0100

    fix: ion-select dev build fix

--- bin/generate-app-configs.js
+++ bin/generate-app-configs.js
@@ -2,7 +2,7 @@ const { environment } = require('../env.json')
 const fs = require('fs')
 
 const ANDROID_SOURCE = './android/app/google-services.json'
-const IOS_SOURCE = './ios/app/GoogleService-Info.plist'
+const IOS_SOURCE = './ios/App/App/GoogleService-Info.plist'
 
 const ANDROID_CONFIG = `./bin/config/${environment}/google-services.json`
 const IOS_CONFIG = `./bin/config/${environment}/GoogleService-Info.plist`
