daa1ca8947749f107008686dacb1a1f98a9d3d84
kromate <akpananthony33@gmail.com>
2022-03-10 00:48:30 +0100

    feat: done with flashcards

--- src/application/components/study/flashCards/FlashCardListView.vue
+++ src/application/components/study/flashCards/FlashCardListView.vue
@@ -1,17 +1,17 @@
 <template>
-	<div id="screen" class="lg:w-8/12 w-full md:px-4 mx-auto mt-8 mb-16">
-		<ion-text class="text-main text-main_dark font-bold heading px-4">
+	<div id="screen" class="lg:w-8/12 w-full md:px-4 mx-auto mt-2  mb-6">
+		<ion-text class="text-main text-main_dark font-bold heading px-4 hidden md:block">
 			All cards info <span class="text-gray">({{ formatNumber(flashCard.set.length) }})</span>
 		</ion-text>
 
 		<div v-for="(card, index) in flashCard.set" :key="index"
 			class="flex justify-between w-full md:mt-4  text-body">
 			<div class="bg-white md:py-3 md:px-6 rounded-xl w-full flex">
-				<DisplayHtml :html="card.question" class="w-1/2 md:p-0 p-2 border-t md:border-0 border-faded_gray" />
+				<DisplayHtml :html="card.question" class="w-1/2 md:p-0 p-2 border-t md:border-0 border-line" />
 
-				<div class="h-auto bg-faded_gray w-[1px] md:-my-3 "/>
+				<div class="h-auto bg-line w-[1px] md:-my-3 "/>
 
-				<DisplayHtml :html="card.answer" class="w-1/2 md:pl-4 p-2  border-t md:border-0 border-faded_gray "/>
+				<DisplayHtml :html="card.answer" class="w-1/2 md:pl-4 p-2  border-t md:border-0 border-line "/>
 			</div>
 		</div>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,44 @@
 module.exports = {
-	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	darkMode: false, // or 'media' or 'class'
-	mode: 'jit',
+	mode: "jit",
 	theme: {
 		screens: {
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1280px',
-			'xl': '1420px',
-			'2xl': '1536px'
+			sm: "640px",
+			md: "768px",
+			lg: "1280px",
+			xl: "1420px",
+			"2xl": "1536px",
 		},
 		extend: {
 			colors: {
 				// Build your palette here
-				primary: '#546DD3',
-				white: '#FEFEFE',
-				black: '#111B25',
-				cyan: '#54B5D3',
-				gray: '#64778A',
-				red: '#FF6666',
-				pink: '#FF6496',
-				yellow_star: '#FFD11C',
-				light_gray: '#FCFCFC',
-				dark_gray: '#4D5C6F',
-				new_gray: '#F2F3F5',
-				icon_inactive: '#8B9EB1',
-				faded_gray: '#C7D6E3',
-				main_dark: '#132740',
-				purple: '#C864DC',
-				orange: '#FFA84B',
-				green: '#00D246'
+				primary: "#546DD3",
+				white: "#FEFEFE",
+				black: "#111B25",
+				cyan: "#54B5D3",
+				gray: "#64778A",
+				red: "#FF6666",
+				line: "#EBECEE",
+				pink: "#FF6496",
+				yellow_star: "#FFD11C",
+				light_gray: "#FCFCFC",
+				dark_gray: "#4D5C6F",
+				new_gray: "#F2F3F5",
+				icon_inactive: "#8B9EB1",
+				faded_gray: "#C7D6E3",
+				main_dark: "#132740",
+				purple: "#C864DC",
+				orange: "#FFA84B",
+				green: "#00D246",
 			},
 			fontFamily: {
-				'rale': ['Raleway', 'sans-serif']
-			}
-		}
+				rale: ["Raleway", "sans-serif"],
+			},
+		},
 	},
 	variants: {
-		extend: {}
+		extend: {},
 	},
-	plugins: []
-}
+	plugins: [],
+};
