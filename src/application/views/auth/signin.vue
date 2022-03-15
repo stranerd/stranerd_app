99fb5252c26fd546cc796d3e74da6cbd635796bb
kevinand11 <kevinfizu@gmail.com>
2022-03-15 11:29:02 +0100

    fix: email bugs

--- src/application/views/auth/signin.vue
+++ src/application/views/auth/signin.vue
@@ -10,9 +10,9 @@
 				<div class="h-[65%]">
 					<form class="flex flex-col gap-4" @submit.prevent="signin">
 						<div>
-							<ion-input v-model="factory.email" :size="24" placeholder="Email Address"
-								position="floating"
-								inputmode="email" />
+							<ion-input v-model.trim="factory.email" :size="24" inputmode="email"
+								placeholder="Email Address"
+								position="floating" />
 							<DisplayError :error="factory.errors.email" />
 						</div>
 						<div>
--- src/application/views/auth/signup.vue
+++ src/application/views/auth/signup.vue
@@ -22,8 +22,8 @@
 							<DisplayError :error="factory.errors.last" />
 						</div>
 						<div>
-							<ion-input v-model="factory.email" :size="24" placeholder="Email Address"
-								position="floating" inputmode="email" />
+							<ion-input v-model.trim="factory.email" :size="24" inputmode="email"
+								placeholder="Email Address" position="floating" />
 							<DisplayError :error="factory.errors.email" />
 						</div>
 						<div>
