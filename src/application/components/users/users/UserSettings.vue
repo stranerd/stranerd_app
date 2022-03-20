1e6e26a8a8f12fafe25d8f410a625cd5050433e1
kromate <akpananthony33@gmail.com>
2022-03-20 22:54:18 +0100

    feat: setting page

--- src/application/components/users/users/UserSettings.vue
+++ src/application/components/users/users/UserSettings.vue
@@ -1,75 +1,74 @@
 <template>
-	<div class="w-full px-4">
-		<div class="bg-primary h-24 -mx-4 hidden md:block" />
+	<div class="w-full lg:w-8/12 mx-auto lg:mt-6">
+		<!-- <div class="bg-primary h-24  hidden md:block" /> -->
 
 		<PageLoading v-if="profileLoading" />
 		<PageLoading v-if="passwordLoading" />
+		<div class="flex flex-col w-full md:rounded-lg border-bottom-line md:pt-8 pt-4  bg-white ">
+			<ion-text class="text-main_dark font-bold md:mb-8 mb-4 md:pl-8 pl-4">Edit profile</ion-text>
+			<CoverAvatar :src="user.bio.coverPhoto" class=" h-11 md:h-28" />
+			<div class="bg-white md:rounded-b-xl px-4 md:px-8 flex flex-col pb-6">
+				<span class="relative">
+					<img :src="photoLink || DEFAULT_PROFILE_PHOTO"
+						alt=""
+						class="-mt-10 md:-mt-12 md:w-24 md:h-24 w-20 h-20 rounded-full">
+					<FileInput accept="image/*"
+						class="rounded-full absolute left-14 bottom-2 h-6 w-6 bg-gray text-white flex items-center justify-center"
+						@files="catchPhoto">
+						<IonIcon :icon="pencilOutline" />
+					</FileInput>
+				</span>
+				<ion-text class="text-heading text-main_dark font-bold text-left w-full mt-7 mb-2">
+					Name
+				</ion-text>
+				<div class="flex flex-row items-center justify-center  w-full">
+					<div class="border border-faded_gray rounded-lg w-1/2 md:mr-4  mr-2">
+						<IonInput v-model="profileFactory.first" class="w-full font-medium mr-2" placeholder="First name" />
+					</div>
+					<div class="border border-faded_gray rounded-lg w-1/2  ">
+						<IonInput v-model="profileFactory.last" class="w-full font-medium" placeholder="Last name" />
+					</div>
+				</div>
 
-		<div
-			class="custom-shadow bg-white lg:w-7/12 p-6 mx-auto flex flex-col items-center justify-center rounded-xl mb-6 md:-mt-12 mt-6">
-			<ion-text class="text-heading text-main_dark font-bold mt-2">
-				Profile Picture
-			</ion-text>
-			<img :src="photoLink || DEFAULT_PROFILE_PHOTO"
-				alt=""
-				class="mt-3 w-24 h-24 rounded-full">
-			<FileInput accept="image/*" class="mt-4" @files="catchPhoto">
-				<label class="bg-primary text-white py-3 px-6 rounded-xl font-bold">
-					Upload a profile picture
-				</label>
-			</FileInput>
-		</div>
-
-		<div
-			class="custom-shadow bg-white lg:w-7/12 p-6 mx-auto flex flex-col items-center justify-center rounded-xl mb-6">
-			<ion-text class="text-main_dark text-left w-full mt-2">
-				<span class="text-heading font-bold">Full names</span> *Use your real names, as they will be used to
-				identify you throughout the platform
-			</ion-text>
-			<div class="flex md:flex-row items-center justify-center flex-col w-full mt-2">
-				<div class="border border-faded_gray rounded-xl lg:w-1/2 md:mr-4 w-full mt-4">
-					<IonInput v-model="profileFactory.first" class="w-full font-medium" placeholder="First name" />
+				<ion-text class="text-heading text-main_dark font-bold text-left w-full mt-4">
+					Bio
+				</ion-text>
+				<div class="flex md:flex-row items-center justify-center flex-col w-full mt-2">
+					<div class="border border-faded_gray rounded-lg w-full">
+						<IonTextarea v-model="profileFactory.description"
+							class="bg-white border-0 focus:outline-none w-full"
+							placeholder="Tell us a little more about yourself"
+							rows="6" />
+					</div>
 				</div>
-				<div class="border border-faded_gray rounded-xl lg:w-1/2 w-full mt-4">
-					<IonInput v-model="profileFactory.last" class="w-full font-medium" placeholder="Last name" />
+				<div class="w-full justify-start mt-2">
+					<ion-button class="btn-primary w-32" @click="updateProfile">
+						Save
+						<IonSpinner v-if="profileLoading" name="lines-small" />
+					</ion-button>
 				</div>
-			</div>
-			<ion-text class="text-heading text-main_dark font-bold text-left w-full mt-4">
-				About
-			</ion-text>
-			<div class="flex md:flex-row items-center justify-center flex-col w-full mt-2">
-				<div class="border border-faded_gray rounded-xl w-full">
-					<IonTextarea v-model="profileFactory.description"
-						class="bg-white border-0 focus:outline-none w-full"
-						placeholder="Tell us a little more about yourself"
-						rows="6" />
-				</div>
-			</div>
-			<div class="w-full justify-start mt-2">
-				<ion-button class="btn-primary w-32" @click="updateProfile">
-					Save
-					<IonSpinner v-if="profileLoading" name="lines-small" />
-				</ion-button>
+			
 			</div>
 		</div>
 
+
 		<div
-			class="custom-shadow bg-white lg:w-7/12 p-6 mx-auto flex flex-col items-center justify-center rounded-xl mb-6">
+			class="bg-white md:mt-4 p-6 mx-auto flex flex-col items-center justify-center rounded-lg mb-6">
 			<ion-text class="text-heading text-main_dark font-bold text-left w-full">
 				{{ hasPassword ? 'Update Password' : 'Add Password' }}
 			</ion-text>
-			<div v-if="hasPassword" class="border border-faded_gray rounded-xl w-full mt-4">
+			<div v-if="hasPassword" class="border border-faded_gray rounded-lg w-full mt-4">
 				<IonInput v-model="passwordFactory.oldPassword" class="w-full font-medium"
 					placeholder="Confirm Old Password"
 					type="password" />
 			</div>
 			<div class="flex md:flex-row gap-4 items-center justify-center flex-col w-full mt-4">
-				<div class="border border-faded_gray rounded-xl w-full">
+				<div class="border border-faded_gray rounded-lg w-full">
 					<IonInput v-model="passwordFactory.password" class="w-full font-medium"
 						placeholder="Enter New Password"
 						type="password" />
 				</div>
-				<div class="border border-faded_gray rounded-xl w-full">
+				<div class="border border-faded_gray rounded-lg w-full">
 					<IonInput v-model="passwordFactory.cPassword" class="w-full font-medium"
 						placeholder="Confirm New Password"
 						type="password" />
@@ -82,23 +81,85 @@
 				</ion-button>
 			</div>
 		</div>
+		
+
+		<div
+			class="bg-white md:mt-4 p-6 mx-auto flex flex-col items-center justify-center rounded-lg mb-6">
+
+			<div class="flex flex-col gap-4 w-full">
+				<ion-radio-group value="ss" class="flex flex-col items-start">
+					<ion-label class="font-bold mb-4">Choose your academic level *</ion-label>
+
+					<div class="flex items-center mt-4">
+						<ion-radio  value="ss" class="mr-4"></ion-radio>
+						<ion-label class="!text-gray">Secondary school</ion-label>
+					</div>			
+					<div class="flex items-center mt-4">
+						<ion-radio  value="ssl" class="mr-4"></ion-radio>
+						<ion-label class="!text-gray">Secondary school leaver</ion-label>
+					</div>			
+					<div class="flex items-center mt-4">
+						<ion-radio  value="uni" class="mr-4"></ion-radio>
+						<ion-label class="!text-gray">University</ion-label>
+					</div>			
+				</ion-radio-group>
+
+
+				<div class="flex w-full justify-between gap-4 mt-4">
+					<ion-button class="btn-primary w-[7.5rem]" type="submit">
+						Save
+					</ion-button>
+				</div>
+	
+
+			</div>
+
+		</div>
+
+		<div
+			class="bg-white md:mt-4 p-6 mx-auto flex flex-col items-center justify-center rounded-lg mb-6">
+
+			<div class="flex flex-col gap-4 w-full">
+				<div class="flex flex-col items-start">
+					<ion-label class="font-bold mb-4">What exam(s) are you studying for?</ion-label>
+					<ion-select v-model="answered"
+						class="bg-white md:bg-new_gray !text-gray flex-grow w-full text-left font-bold select-primary"
+						interface="action-sheet"
+						placeholder="Select exam(s)">
+						<ion-select-option v-for="choice in 5" :key="choice"
+							:value="choice" @click="answered = choice">
+							{{ choice }}
+						</ion-select-option>
+					</ion-select>
+				</div>
+	
+				<div class="flex w-full justify-start gap-4">			
+					<ion-button  class="btn-primary w-[7.5rem]" type="submit">
+						Save
+					</ion-button>
+				</div>
+			</div>
+
+		</div>
+
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { IonButton, IonInput, IonSpinner, IonTextarea } from '@ionic/vue'
-import { image } from 'ionicons/icons'
+import { IonButton, IonInput, IonSpinner, IonTextarea, IonRadio, IonRadioGroup } from '@ionic/vue'
+import { image, pencilOutline } from 'ionicons/icons'
 import { useProfileUpdate } from '@app/composable/auth/profile'
 import { useAuth } from '@app/composable/auth/auth'
 import { useFileInputCallback } from '@app/composable/core/forms'
 import { usePasswordUpdate } from '@app/composable/auth/passwords'
 import { DEFAULT_PROFILE_PHOTO } from '@utils/constants'
 
+	
 export default defineComponent({
 	name: 'ProfileSettings',
 	components: {
-		IonTextarea,
+		IonTextarea,IonRadio, IonRadioGroup,
 		IonSpinner,
 		IonButton,
 		IonInput
@@ -140,7 +201,7 @@ export default defineComponent({
 
 		return {
 			user,
-			DEFAULT_PROFILE_PHOTO,
+			DEFAULT_PROFILE_PHOTO, pencilOutline,
 			profileFactory,
 			profileError,
 			profileLoading,
@@ -163,9 +224,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-	.custom-shadow {
-		box-shadow: 0 0 25px rgba(77, 92, 111, 0.1);
-	}
 
 	ion-button {
 		--background: #546DD3;
--- src/application/views/study/notes/create.vue
+++ src/application/views/study/notes/create.vue
@@ -4,13 +4,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useOnboardingModal } from '@app/composable/core/modals'
+import { useStudyModal } from '@app/composable/core/modals'
 
 export default defineComponent({
 	name: 'StudyNotesCreate',
 	displayName: 'Create Note',
 	middlewares: ['isAuthenticated', async ({ from }) => {
-		useOnboardingModal().openSetupAccount()
+		useStudyModal().openCreateNote()
 		const backPath = from?.fullPath ?? '/dashboard'
 		return backPath.startsWith('/auth/') ? '/dashboard' : backPath
 	}]
