69dbfcce5a2df5a55ef9c958a839a5c6dcc8f82d
kevinand11 <kevinfizu@gmail.com>
2022-05-21 15:23:40 +0100

    fix: reset password

--- src/application/views/auth/reset.vue
+++ src/application/views/auth/reset.vue
@@ -1,51 +1,55 @@
 <template>
-	<ion-page>
-		<ion-content>
-			<div class="w-full mt-10 h-full flex flex-col items-center justify-start py-20">
-				<div class="flex flex-col items-center justify-center p-10 lg:bg-light_gray">
-					<h1 class="text-xl text-main_dark font-bold mb-2 ">Reset Password</h1>
-					<span
-						class="text-main_dark mb-4">To reset your password, type your new password below and reset</span>
-					<div class="h-[65%]">
-						<form
-							@submit.prevent="resetPassword"
-						>
-							<div class="mb-4">
-								<ion-input v-model="factory.password" :size="24" placeholder="New Password"
-									position="floating"
-									required type="password"></ion-input>
-								<DisplayError :error="factory.errors.password" />
-							</div>
-							<div class="mb-4">
-								<ion-input v-model="factory.cPassword" :size="24" placeholder="Confirm New Password"
-									position="floating" required type="password" />
-								<DisplayError :error="factory.errors.cPassword" />
-							</div>
-							<ion-button class="w-full mb-4" type="submit">Reset Password
-								<BlockLoading v-if="loading" />
-							</ion-button>
-						</form>
-					</div>
-					<router-link class="text-primary font-bold mt-8" to="/auth/signin">
-						Back to Sign In
-					</router-link>
-				</div>
+	<Auth>
+		<div class="w-full h-full flex">
+			<div class="w-7/12 lg:flex flex-col items-center justify-center h-full hidden">
+				<h1 class="text-5xl font-extrabold mb-12">Reset Password</h1>
+				<img alt="" class="object-contain h-[65%]" src="@app/assets/images/auth/auth.png">
 			</div>
-			<page-loading v-if="loading" />
-		</ion-content>
-	</ion-page>
+			<div class="flex flex-col items-center mt-6 md:justify-center lg:w-5/12 w-full p-4">
+				<form class="h-[65%] w-full md:w-[70%] flex flex-col gap-4" @submit.prevent="resetPassword">
+					<div class="flex flex-col">
+						<ion-label class="font-bold text-sm mb-2">Password</ion-label>
+						<ion-input v-model="factory.password"
+							:class="{'valid': factory.isValid('password'), 'invalid': factory.errors.password}"
+							:size="24"
+							placeholder="Enter new password" position="floating" type="password" />
+						<DisplayError :error="factory.errors.password" />
+					</div>
+					<div class="flex flex-col">
+						<ion-label class="font-bold text-sm mb-2">Confirm Password</ion-label>
+						<ion-input v-model="factory.cPassword"
+							:class="{'valid': factory.isValid('cPassword'), 'invalid': factory.errors.cPassword}"
+							:size="24"
+							placeholder="Confirm new password" position="floating" type="password" />
+						<DisplayError :error="factory.errors.cPassword" />
+					</div>
+					<ion-button :disabled="loading || !factory.valid" class="w-full text-sm btn-primary mt-2"
+						type="submit">
+						<SpinLoading v-if="loading" />
+						<span v-else>Reset</span>
+					</ion-button>
+					<div class="w-full flex justify-center items-center">
+						<router-link class="text-primaryBg" to="/auth/signin">
+							Back to Sign In
+						</router-link>
+					</div>
+				</form>
+			</div>
+		</div>
+	</Auth>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { usePasswordReset } from '@app/composable/auth/passwords'
-import { IonButton, IonContent, IonInput, IonPage } from '@ionic/vue'
+import { IonButton, IonInput } from '@ionic/vue'
+import Auth from '@app/layouts/Auth.vue'
 import { useRoute } from 'vue-router'
 
 export default defineComponent({
 	name: 'AuthReset',
 	displayName: 'Reset Password',
-	components: { IonContent, IonPage, IonInput, IonButton },
+	components: { Auth, IonInput, IonButton },
 	middlewares: ['hasQueryToken'],
 	setup () {
 		const { token } = useRoute().query
