cb9a286b0b69f963cc8c4b6eaf647589d95e4d2d
kromate <akpananthony33@gmail.com>
2022-05-29 02:45:17 +0100

    feat: update

--- src/application/components/classes/timetable/TimetableList.vue
+++ src/application/components/classes/timetable/TimetableList.vue
@@ -1,20 +1,24 @@
 <template>
 	<div class="showcase-flex">
-		<div class="flex justify-end px-4 md:px-0">
-			<ion-button class="btn-primary" @click="openTimetableModal(classInst)">
-				Setup Timetable
-			</ion-button>
+		<div class="flex justify-between items-center px-4 md:px-0 py-4 border-bottom-line" @click="openTimetableModal(classInst)">
+			<ion-text>Set up timetable</ion-text>
+			<IonIcon :icon="arrowForwardOutline" class="text-heading2 text-bodyText" />
+
+
 		</div>
 		<div class="flex items-center gap-2 w-full overflow-x-auto p-4 md:p-0">
 			<a v-for="day in days" :key="day.day"
-				:class="activeDay === day.day ? 'bg-primary text-white border-primary' : 'text-gray bg-white border-gray md:border-none'"
+				:class="activeDay === day.day ? 'bg-primaryBg text-primaryText' : 'text-secondaryText bg-bodyBg border-itemBg md:border-none'"
 				class="w-full py-2 px-4 rounded-lg text-center border"
 				@click="activeDay = day.day">
-				{{ day.name }}
+				{{ day.name }} 
 			</a>
 		</div>
-		<div class="flex flex-col md:gap-4">
-			<div v-for="event in timetable" :key="event.hash"
+		<div class="flex flex-col md:gap-4 px-4">
+			<div class="flex flex-col min-h-[96px] rounded-lg border-l-8 border-primaryBg">
+				<ion-text>{{ event.title }}</ion-text>
+			</div>
+			<!-- <div v-for="event in timetable" :key="event.hash"
 				class="card-padding bg-white rounded-xl flex gap-1 items-center">
 				<span class="bg-new_gray text-gray py-1 px-2 rounded-lg text-sub">
 					{{
@@ -26,7 +30,7 @@
 					}}:{{ event.data.end.minute.toString().padStart(2, '0') }}
 				</span>
 				<span class="text-dark">{{ event.title }}</span>
-			</div>
+			</div> -->
 		</div>
 		<PageLoading v-if="loading" />
 	</div>
@@ -36,6 +40,7 @@
 import { computed, defineComponent, ref } from 'vue'
 import { ClassEntity, EventType } from '@modules/classes'
 import { openTimetableModal, useTimetable } from '@app/composable/classes/timetable'
+import { arrowForwardOutline } from 'ionicons/icons'
 
 export default defineComponent({
 	name: 'TimetableList',
@@ -57,7 +62,7 @@ export default defineComponent({
 			.filter((e) => e.data.type === EventType.timetable && e.data.start.day === activeDay.value))
 		return {
 			loading, error, openTimetableModal,
-			activeDay, days, timetable
+			activeDay, days, timetable, arrowForwardOutline
 		}
 	}
 })
---
+++ src/application/views/classes/_classId/timetable/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<ClassWrapper>
+		<template v-slot="{ classInst }">
+			<TimetableList :classInst="classInst" />
+		</template>
+	</ClassWrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ClassWrapper from '@app/components/classes/classes/ClassWrapper.vue'
+import TimetableList from '@app/components/classes/timetable/TimetableList.vue'
+
+export default defineComponent({
+	name: 'ClassClassIdTimetable',
+	displayName: 'Timetable',
+	components: { TimetableList, ClassWrapper }
+})
+</script>
+
