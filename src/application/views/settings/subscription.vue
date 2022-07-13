ac852ce0297edb7d08f02cc44e658d81fb791c1f
kromate <akpananthony33@gmail.com>
2022-07-13 20:03:47 +0100

    feat: sub update

--- src/application/views/settings/subscription.vue
+++ src/application/views/settings/subscription.vue
@@ -4,6 +4,29 @@
 			<SettingsPanel />
 		</template>
 
+		<div class="flex flex-col border-bottom-line gap-4 p-4 lg:p-0">
+			<IonText>Current Plan</IonText>
+			<IonText class="text-2xl  font-bold">Basic Free</IonText>
+		</div>
+
+		<router-link class="border-bottom-line p-4 lg:p-0 flex items-center md:mt-12"
+			to="#">
+			<IonText>See available plans</IonText>
+		</router-link>
+
+		<div class="flex flex-col border-bottom-line gap-7 p-4 lg:p-0 md:mt-12">
+			<IonText class="font-bold">Payment method</IonText>
+			<span class="flex items-center gap-2">
+				<IonIcon :icon="cardOutline" />
+				<IonText>**** **** **** 3227</IonText>
+			</span>
+			<router-link class=" flex items-center gap-4"
+				to="#">
+				<IonIcon :icon="addOutline" />
+				<IonText>Add debit/credit card</IonText>
+			</router-link>
+		</div>
+
 	</DefaultLayout>
 </template>
 
@@ -12,14 +35,15 @@ import { defineComponent } from 'vue'
 import { generateMiddlewares } from '@app/middlewares'
 import { useRouteMeta } from '@app/composable/core/states'
 import SettingsPanel from '@app/components/layout/panels/SettingsPanel.vue'
+import { addOutline, cardOutline } from 'ionicons/icons'
 
 export default defineComponent({
-	name: 'SettingsSecurity',
+	name: 'SettingsSubscription',
 	components: { SettingsPanel },
 	beforeRouteEnter: generateMiddlewares(['isAuthenticated']),
 	setup () {
 		useRouteMeta('Subscription', { back: true })
-	
+		return {addOutline,cardOutline}
 	}
 })
 </script>
