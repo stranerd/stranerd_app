f6bc5e99bb1f97abc3122f1c3c64909bf0998fb5
Abubakri Sherif <saykeed@gmail.com>
2022-09-01 19:55:58 +0100

    feat: created the contact page vue file

---
+++ src/application/views/contact.vue
@@ -0,0 +1,19 @@
+<template>
+	<HomeLayout>
+		<Contact />
+	</HomeLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouteMeta } from '@app/composable/core/states'
+import Contact from '@app/components/landing/contact/index.vue'
+
+export default defineComponent({
+	name: 'PageContact',
+	components: { Contact },
+	setup () {
+		useRouteMeta('Contact', {})
+	}
+})
+</script>
