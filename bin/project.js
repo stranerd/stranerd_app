2b723413522634f8d56607802ebcd44fe32cba2f
kevinand11 <kevinfizu@gmail.com>
2022-10-02 00:09:26 +0100

    chore: project ios build step

--- bin/project.js
+++ bin/project.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 const { MobileProject } = require('@trapezedev/project')
 const fs = require('fs')
+const { exec } = require('child_process')
 const envs = require('../env.json')
 const { asset_links, google_client_ids, package_name, app_name, environment, domain } = envs
+const isProduction = environment === 'production'
 
 const getProject = async () => {
 	const project = new MobileProject('./', {
@@ -12,7 +15,12 @@ const getProject = async () => {
 	return project
 }
 
-const setup = async () => {
+const copyTo = (from, to, error) => {
+	if (fs.existsSync(from)) fs.createReadStream(from).pipe(fs.createWriteStream(to))
+	else return console.log(error)
+}
+
+const setup = async (args) => {
 	const folder = './public/.well-known'
 	if (!fs.existsSync(folder)) fs.mkdirSync(folder, { recursive: true })
 	fs.writeFileSync(`${ folder }/assetlinks.json`, JSON.stringify(asset_links.android ?? {}, null, 4))
@@ -25,8 +33,8 @@ const setup = async () => {
 	const versionCode = 1
 	const versionName = '0.0.0'
 
-	const targets = project.ios.getTargets()
-	await Promise.all(targets.map(async (target) => {
+	const iosTeamId = 'SK23HB744S'
+	await Promise.all(project.ios.getTargets().map(async (target) => {
 		await Promise.all(target.buildConfigurations.map(async (build) => {
 			await project.ios.setBundleId(target.name, build.name, package_name)
 			await project.ios.setDisplayName(target.name, build.name, app_name)
@@ -34,8 +42,12 @@ const setup = async () => {
 			await project.ios.setBuild(target.name, build.name, versionCode)
 			await project.ios.setBuildProperty(target.name, build.name, 'GOOGLE_CLIENT_ID', reversedIosClientId)
 			await project.ios.setBuildProperty(target.name, build.name, 'DOMAIN', domain)
+			await project.ios.setBuildProperty(target.name, build.name, 'CODE_SIGN_STYLE', 'Manual')
+			await project.ios.setBuildProperty(target.name, build.name, 'IPHONEOS_DEPLOYMENT_TARGET', '13.0')
+			const identity = isProduction && build.name === 'Release' ? 'Apple Distribution' : 'Apple Development'
+			await project.ios.setBuildProperty(target.name, build.name, 'CODE_SIGN_IDENTITY', identity)
+			await project.ios.setBuildProperty(target.name, build.name, 'DEVELOPMENT_TEAM', iosTeamId)
 			await project.ios.addEntitlements(target.name, build.name, {
-				'aps-environment': environment,
 				'com.apple.developer.associated-domains': [`applinks:${ domain }`]
 			})
 		}))
@@ -47,25 +59,25 @@ const setup = async () => {
 
 	await project.commit()
 
-	const ANDROID_SOURCE = './android/app/google-services.json'
-	const IOS_SOURCE = './ios/App/App/GoogleService-Info.plist'
+	const googleServicesPath = './android/app/google-services.json'
+	const googlePlistPath = './ios/App/App/GoogleService-Info.plist'
+	const exportPlistPath = './ios/App/App/Export.plist'
 
-	const ANDROID_CONFIG = `./bin/config/${ environment }/google-services.json`
-	const IOS_CONFIG = `./bin/config/${ environment }/GoogleService-Info.plist`
+	const googleServicesConfig = `./bin/config/${ environment }/google-services.json`
+	const googlePlistConfig = `./bin/config/${ environment }/GoogleService-Info.plist`
+	const exportPlistConfig = `./bin/config/${ environment }/Export.plist`
 
-	const args = process.argv.slice(3)
 	if (args.includes('--skip-apps')) return
 
-	if (fs.existsSync(ANDROID_CONFIG)) fs.createReadStream(ANDROID_CONFIG).pipe(fs.createWriteStream(ANDROID_SOURCE))
-	else return console.log('Provide a google-services.json file for your current environment')
-	if (fs.existsSync(IOS_CONFIG)) fs.createReadStream(IOS_CONFIG).pipe(fs.createWriteStream(IOS_SOURCE))
-	else return console.log('Provide a GoogleService-Info.plist file for your current environment')
+	copyTo(googleServicesConfig, googleServicesPath, 'Provide a google-services.json file for your current environment')
+	copyTo(googlePlistConfig, googlePlistPath, 'Provide a GoogleService-Info.plist file for your current environment')
+	copyTo(exportPlistConfig, exportPlistPath, 'Provide a Export.plist file for your current environment')
 }
 
-const version = async () => {
+const version = async (args) => {
 	const firstVersionDate = 1640995200000
 	const versionCode = Math.floor((Date.now() - firstVersionDate) / 1e5)
-	const versionName = process.argv[3].toString()
+	const versionName = args[0].toString()
 	if (!versionName) return console.log('Provide a version please')
 
 	const project = await getProject()
@@ -92,8 +104,45 @@ const env = async () => {
 	fs.writeFileSync('.env', envFormattedEntries)
 }
 
-const type = process.argv[2]
+// TODO: figure how to install provisioningFile
+const appBuild = async (args) => {
+	const platform = args[0]
+	const configuration = args[1] ?? 'Release'
+	const validConfigurations = ['Release', 'Debug']
 
-if (type === 'setup') setup().then()
-else if (type === 'version') version().then()
-else if (type === 'env') env().then()
+	if (!validConfigurations.includes(configuration)) {
+		console.log(`Invalid configuration. Supported values: ${validConfigurations.join(', ')}`)
+		process.exit(1)
+	}
+
+	const ios = async () => {
+		const archiveFile = 'Archive/App'
+		const ipaFile = 'Output'
+		const exportPlistFile = 'App/Export.plist'
+
+		return `cd ios/App &&
+xcodebuild archive -workspace App.xcworkspace -configuration ${configuration} -scheme App -archivePath ${archiveFile} &&
+xcodebuild -exportArchive -archivePath ${archiveFile}.xcarchive -exportPath ${ipaFile} -exportOptionsPlist ${exportPlistFile}
+`
+	}
+
+	const platforms = {
+		ios,
+		default: () => 'echo Invalid platform provided'
+	}
+
+	const runner = platforms[platform] ?? platforms.default
+	exec(runner(), { maxBuffer: 1024 * 1024 * 5 }, (error, stdout, stderr) => {
+		if (error) return console.log(error.message)
+		if (stderr) return console.log(stderr)
+		console.log(stdout)
+	})
+}
+
+const actions = {
+	setup, version, env, appBuild,
+	default: async () => console.log('Invalid action provided')
+}
+
+const run = actions[process.argv[2]] ?? actions.default
+run(process.argv.slice(3)).then()
