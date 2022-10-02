ff5ad93707ba64f35725ae16781f7f384e2c74d5
kevinand11 <kevinfizu@gmail.com>
2022-10-02 02:52:50 +0100

    chore: project android build step documentation

--- Readme.md
+++ Readme.md
@@ -1,4 +1,30 @@
-## For IOS
+# Getting Started
+```bash
+$ git clone https://github.com/stranerd/stranerd_app
+$ cd stranerd_app
+$ yarn install
 
-- Uninstall pod if installed through gem `sudo gem uninstall cocoapods`
-- Install cocoapods through homebrew `homebrew install cocoapods`
+# To run web in dev mode
+$ yarn serve
+
+# To run android in dev mode
+$ yarn run:android
+
+# To run ios in dev mode
+$ yarn run:ios
+
+# To build web
+$ yarn build:web
+
+# To build android
+$ yarn bin:project:appBuild android Release|Debug bundle|assemble
+
+# To build ios
+$ yarn bin:project:appBuild ios Release|Debug
+```
+
+## Dev Tools
+- git
+- node && npm/yarn
+- xcode && xcodebuild (to build and sign ios app) (Comes with an installation of XCode. Make sure it is available in your PATH)
+- apksigner && zipalign (to build and sign android app) (Comes with an installation of Android Studio. Make sure it is available in your PATH)
--- bin/project.js
+++ bin/project.js
@@ -106,37 +106,67 @@ const env = async () => {
 
 // TODO: figure how to install provisioningFile
 const appBuild = async (args) => {
+	const validPlatforms = ['android', 'ios']
 	const platform = args[0]
-	const configuration = args[1] ?? 'Release'
+
+	if (!validPlatforms.includes(platform)) {
+		console.log(`Invalid platform. Supported values: ${validPlatforms.join(', ')}`)
+		process.exit(1)
+	}
+
 	const validConfigurations = ['Release', 'Debug']
+	const configuration = args[1] ?? validConfigurations[0]
 
 	if (!validConfigurations.includes(configuration)) {
 		console.log(`Invalid configuration. Supported values: ${validConfigurations.join(', ')}`)
 		process.exit(1)
 	}
 
-	const ios = async () => {
+	const android = () => {
+		const validTypes = ['assemble', 'bundle']
+		const type = args[2] ?? validTypes[0]
+
+		if (!validTypes.includes(type)) {
+			console.log(`Invalid type. Supported values: ${validTypes.join(', ')}`)
+			process.exit(1)
+		}
+
+		const isAssemble = type === validTypes[0]
+		const isBundle = type === validTypes[1]
+
+		const sign = 'apksigner sign --ks ./stranerd.keystore --ks-key-alias kevin@stranerd.com'
+		const signAssemble = `zipalign 4 ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk && ${sign} ./app/build/outputs/apk/release/app-release.apk`
+		const signBundle = `${sign} --min-sdk-version 22 ./app/build/outputs/bundle/release/app-release.aab`
+
+		const install = args.includes('--install') ? `./gradlew install${configuration} &&` : ''
+
+		return `cd android && rm -rf app/build &&
+./gradlew ${type + configuration} && ${install}
+${isAssemble ? signAssemble : ''}${isBundle ? signBundle : ''}
+`
+	}
+
+	const ios = () => {
 		const archiveFile = 'Archive/App'
 		const ipaFile = 'Output'
 		const exportPlistFile = 'App/Export.plist'
 
-		return `cd ios/App &&
+		return `cd ios/App && rm -rf ${ipaFile} &&
 xcodebuild archive -workspace App.xcworkspace -configuration ${configuration} -scheme App -archivePath ${archiveFile} &&
 xcodebuild -exportArchive -archivePath ${archiveFile}.xcarchive -exportPath ${ipaFile} -exportOptionsPlist ${exportPlistFile}
 `
 	}
 
 	const platforms = {
-		ios,
+		android, ios,
 		default: () => 'echo Invalid platform provided'
 	}
 
 	const runner = platforms[platform] ?? platforms.default
-	exec(runner(), { maxBuffer: 1024 * 1024 * 5 }, (error, stdout, stderr) => {
-		if (error) return console.log(error.message)
-		if (stderr) return console.log(stderr)
-		console.log(stdout)
-	})
+	const proc = exec(runner(), { maxBuffer: 1024 * 1024 * 5 })
+	proc.stdout.pipe(process.stdout)
+	proc.stderr.pipe(process.stderr)
+	process.stdin.pipe(proc.stdin)
 }
 
 const actions = {
