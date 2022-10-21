d78ce6ac445ef90365b0c55a6a54e9c6ca2e0745
kevinand11 <kevinfizu@gmail.com>
2022-10-21 10:50:16 +0100

    fix: classes entity listen route

--- src/modules/classes/data/datasources/groupApi.ts
+++ src/modules/classes/data/datasources/groupApi.ts
@@ -7,40 +7,40 @@ export class GroupApiDataSource implements GroupBaseDataSource {
 	private stranerdClient: HttpClient
 
 	constructor () {
-		this.stranerdClient = new HttpClient(apiBase + '/classes/groups')
+		this.stranerdClient = new HttpClient(apiBase + '/classes')
 	}
 
 	async create (classId: string, data: GroupToModel) {
-		return await this.stranerdClient.post<GroupToModel, GroupFromModel>(`/${classId}`, data)
+		return await this.stranerdClient.post<GroupToModel, GroupFromModel>(`/${classId}/groups`, data)
 	}
 
 	async find (classId: string, id: string) {
-		return await this.stranerdClient.get<any, GroupFromModel>(`/${classId}/${id}`, {})
+		return await this.stranerdClient.get<any, GroupFromModel>(`/${classId}/groups/${id}`, {})
 	}
 
 	async get (classId: string, query: QueryParams) {
-		return await this.stranerdClient.get<QueryParams, QueryResults<GroupFromModel>>(`/${classId}`, query)
+		return await this.stranerdClient.get<QueryParams, QueryResults<GroupFromModel>>(`/${classId}/groups`, query)
 	}
 
 	async listenToOne (classId: string, id: string, listeners: Listeners<GroupFromModel>) {
-		const listener = listenOnSocket(`classes/groups/${classId}/${id}`, listeners)
+		const listener = listenOnSocket(`classes/${classId}/groups/${id}`, listeners)
 		const model = await this.find(classId, id)
 		if (model) await listeners.updated(model)
 		return listener
 	}
 
 	async listenToMany (classId: string, query: QueryParams, listeners: Listeners<GroupFromModel>) {
-		const listener = listenOnSocket(`classes/groups/${classId}`, listeners)
+		const listener = listenOnSocket(`classes/${classId}/groups`, listeners)
 		const models = await this.get(classId, query)
 		await Promise.all(models.results.map(listeners.updated))
 		return listener
 	}
 
 	async delete (classId: string, id: string) {
-		await this.stranerdClient.delete<any, boolean>(`/${classId}/${id}`, {})
+		await this.stranerdClient.delete<any, boolean>(`/${classId}/groups/${id}`, {})
 	}
 
 	async update (classId: string, id: string, data: GroupToModel) {
-		return await this.stranerdClient.put<GroupToModel, GroupFromModel>(`/${classId}/${id}`, data)
+		return await this.stranerdClient.put<GroupToModel, GroupFromModel>(`/${classId}/groups/${id}`, data)
 	}
 }
