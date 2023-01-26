e55abb968480b67ef8acac27f5cd33f922e22c67
kevinand11 <kevinfizu@gmail.com>
2023-01-26 14:45:53 +0100

    refactor: turn flashcard modes to pages

--- src/application/views/study/flashCards/_flashCardId/match.vue
+++ src/application/views/study/flashCards/_flashCardId/match.vue
@@ -1,20 +1,22 @@
 <template>
 	<FlashCardPageWrapper>
 		<template v-slot="{ flashCard }">
-			<FlashCardDetails :flashCard="flashCard" />
+			<Match :close="() => $router.replace(`/study/flashCards/${flashCard.id}`)" :flashCard="flashCard" />
 		</template>
 	</FlashCardPageWrapper>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import FlashCardDetails from '@app/components/study/flashCards/FlashCardDetails.vue'
+import Match from '@app/components/study/flashCards/modes/Match.vue'
 import { useRouteMeta } from '@app/composable/core/states'
 import FlashCardPageWrapper from '@app/components/study/flashCards/FlashCardPageWrapper.vue'
+import { generateMiddlewares } from '@app/middlewares'
 
 export default defineComponent({
-	name: 'StudyFlashCardsFlashcardId',
-	components: { FlashCardPageWrapper, FlashCardDetails },
+	name: 'StudyFlashCardsFlashcardIdMatch',
+	components: { FlashCardPageWrapper, Match },
+	beforeRouteEnter: generateMiddlewares(['isAuthenticated']),
 	setup () {
 		useRouteMeta('Flashcard Set', { back: true })
 	}
--- src/application/views/study/flashCards/_flashCardId/test.vue
+++ src/application/views/study/flashCards/_flashCardId/test.vue
@@ -1,20 +1,22 @@
 <template>
 	<FlashCardPageWrapper>
 		<template v-slot="{ flashCard }">
-			<FlashCardDetails :flashCard="flashCard" />
+			<Test :close="() => $router.replace(`/study/flashCards/${flashCard.id}`)" :flashCard="flashCard" />
 		</template>
 	</FlashCardPageWrapper>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import FlashCardDetails from '@app/components/study/flashCards/FlashCardDetails.vue'
+import Test from '@app/components/study/flashCards/modes/Test.vue'
 import { useRouteMeta } from '@app/composable/core/states'
 import FlashCardPageWrapper from '@app/components/study/flashCards/FlashCardPageWrapper.vue'
+import { generateMiddlewares } from '@app/middlewares'
 
 export default defineComponent({
-	name: 'StudyFlashCardsFlashcardId',
-	components: { FlashCardPageWrapper, FlashCardDetails },
+	name: 'StudyFlashCardsFlashcardIdMatch',
+	components: { FlashCardPageWrapper, Test },
+	beforeRouteEnter: generateMiddlewares(['isAuthenticated']),
 	setup () {
 		useRouteMeta('Flashcard Set', { back: true })
 	}
